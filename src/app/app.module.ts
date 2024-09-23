e658467a09a51f1e8ff0abb8783035ff20e18a55
Mighty T. <anawathd@gmail.com>
2024-09-23 10:28:26 +0700

    update_appcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
-import { ChatCompletionsComponent } from '../features/chat-completions/chat-completions.component';
+import { ChatCompletionsComponent } from './features/chat-completions/chat-completions.component';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
